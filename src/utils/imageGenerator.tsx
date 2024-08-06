e3a5d9fdf472e9a439927b636b9a26aa8c34801d
dadiosy <roberto.song940726@gmail.com>
2024-08-06 14:34:20 -0700

    use join instead of resolve

--- src/utils/imageGenerator.tsx
+++ src/utils/imageGenerator.tsx
@@ -4,9 +4,9 @@ import path from "path";
 import fs from "fs";
 
 async function getGeneratedImages(pageNumber: number, pageCount: number): Promise<{ [fileName: string]: any }> {
-  // const dirPath = path.join(process.cwd(), "public/collections");
+  const dirPath = path.join(process.cwd(), "collections");
   // const dirPath = path.resolve('./public', "collections");
-  const dirPath = path.resolve('./collections');
+  // const dirPath = path.resolve('./collections');
 
   return readJSONFiles(dirPath, pageNumber, pageCount);
 }
@@ -22,9 +22,9 @@ async function readJSONFiles(
 
   try {
     const files = await fs.promises.readdir(
-      // path.join(process.cwd(), "public/collections")
+      path.join(process.cwd(), "collections")
       // path.resolve('./public', "collections")
-      path.resolve('./collections')
+      // path.resolve('./collections')
     );
 
     jsonFiles = files.filter(file => path.extname(file).toLowerCase() === '.json');
@@ -32,8 +32,8 @@ async function readJSONFiles(
     await Promise.all(
       pageFiles.map(async (file) => {
         if (path.extname(file).toLowerCase() === ".json") {
-          // const filePath = path.join(dirPath, file);
-          const filePath = path.resolve(dirPath, file);
+          const filePath = path.join(dirPath, file);
+          // const filePath = path.resolve(dirPath, file);
           const content = await fs.promises.readFile(filePath, "utf-8");
           jsonData.push(JSON.parse(content));
         }
