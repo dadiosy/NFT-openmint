07133dc7bb10fd4cdf0776ea32554f87860e2aee
dadiosy <roberto.song940726@gmail.com>
2024-08-06 14:51:31 -0700

    use next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        // includes files from the monorepo base two directories up
+        outputFileTracingRoot: path.join(__dirname, 'collections'),
+    },
 };
 
 export default nextConfig;
